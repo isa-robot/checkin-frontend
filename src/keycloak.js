8c8ad992a200dbe7871584302e9a6333375dd9aa
erickpoleto <erick-poleto@hotmail.com>
2020-07-24 19:35:33 -0300

    keycloak + env

--- src/keycloak.js
+++ src/keycloak.js
@@ -2,9 +2,18 @@ import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
+
 export const keycloak = new Keycloak();
 
 export const keycloakProviderInitConfig = {
+  "realm": "isa-qualis",
+  "auth-server-url": `${process.env.KEYCLOAK_SERVER_URL}/`,
+  "ssl-required": "external",
+  "resource": `${process.env.KEYCLOAK_CLIENT}`,
+  "public-client": true,
+  "verify-token-audience": true,
+  "use-resource-role-mappings": true,
+  "confidential-port": 0,
   onLoad: 'login-required',
   // onLoad: 'check-sso',
 };
