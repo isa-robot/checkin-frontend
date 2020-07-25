1a697f6637e0ad5f836532a9806df19d71ffee1c
Matheus Frigo <matheus.tfrigo@gmail.com>
2020-07-25 17:21:05 -0300

    Tela de Login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,39 +1,31 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { Divider } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 
 import { useKeycloak } from '@react-keycloak/web';
 
-import {
-  Container,
-  CardFooter,
-  CardHeader,
-  GreenButton,
-  Logo,
-  LogoSecondary,
-  Card,
-} from './styles';
+import { Loading } from './styles';
 import { kcSignInRequest, kcOnAuth } from '~/store/modules/auth/actions';
 
 export default function SignIn() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const { loading } = useSelector(state => state.auth);
-
-  useEffect(() => {
-    if (loading) history.push('/qualis');
-  }, [loading, history]);
 
   const [keycloak, initialized] = useKeycloak();
 
   if (initialized) {
     if (keycloak.authenticated) {
       dispatch(kcOnAuth());
+      history.push('/qualis');
     } else {
       dispatch(kcSignInRequest());
     }
   }
 
-  return <Container />;
+  return (
+    <Loading>
+      <CircularProgress size="5rem" />
+    </Loading>
+  );
 }
