41e2adcd15b807afa357d5d8202be73023537219
Matheus Frigo <matheus.tfrigo@gmail.com>
2020-07-25 17:20:49 -0300

    Telas principais

--- src/pages/Qualis/index.js
+++ src/pages/Qualis/index.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CircularProgress } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { Loading, Container } from './styles';
 
 export default function Qualis() {
   const loaded = useState(true);
+  const { roles } = useSelector(state => state.user.profile);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (roles.includes('assisted')) history.push('/diario');
+  }, [roles, history]);
 
   return (
     <>
       {loaded ? (
-        <Container></Container>
+        <Container />
       ) : (
         <Loading>
           <CircularProgress size="5rem" />
--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
-import { useKeycloak } from '@react-keycloak/web';
-
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
 import NoAccessModal from '~/components/NoAccessModal';
@@ -14,22 +12,22 @@ import { Wrapper, Layer } from './styles';
 export default function DefaultLayout({ children }) {
   const [toggleNoAccess, setToggleNoAccess] = useState(false);
   const [toggleBaseline, setToggleBaseline] = useState(false);
-  const [keycloak] = useKeycloak();
 
-  const { profile } = useSelector(state => state.user);
+  const { baseline, roles, resources } = useSelector(
+    state => state.user.profile
+  );
   const { loading } = useSelector(state => state.auth);
-  // const { roles } = useSelector(state => state.user.profile);
-  const baseline  = profile ? profile.baseline : null;
-  console.log(profile)
-
-  const roles = keycloak.realmAccess ? keycloak.realmAccess.roles : [];
 
   useEffect(() => {
     if (!loading) {
       if (roles.length === 0) setToggleNoAccess(true);
-      else if (baseline === null) setToggleBaseline(true);
+      else {
+        setToggleNoAccess(false);
+        if (resources.includes('diary') && baseline === null)
+          setToggleBaseline(true);
+      }
     }
-  }, [roles, profile, baseline]);
+  }, [roles, resources, baseline, loading]);
 
   function toggleBaselineModal(prop) {
     setToggleBaseline(prop);
