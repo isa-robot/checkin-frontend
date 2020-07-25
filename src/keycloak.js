9cf1503a2ed02dc886182097f9be8571aa1fba01
Matheus Frigo <matheus.tfrigo@gmail.com>
2020-07-25 17:56:25 -0300

    Variáveis de ambiente para rodar em container

--- src/keycloak.js
+++ src/keycloak.js
@@ -6,14 +6,14 @@ import Keycloak from 'keycloak-js';
 export const keycloak = new Keycloak();
 
 export const keycloakProviderInitConfig = {
-  "realm": "isa-qualis",
-  "auth-server-url": `${process.env.KEYCLOAK_SERVER_URL}/`,
-  "ssl-required": "external",
-  "resource": `${process.env.KEYCLOAK_CLIENT}`,
-  "public-client": true,
-  "verify-token-audience": true,
-  "use-resource-role-mappings": true,
-  "confidential-port": 0,
+  realm: 'isa-qualis',
+  'auth-server-url': `${process.env.KEYCLOAK_SERVER_URL}/`,
+  'ssl-required': 'external',
+  resource: `${process.env.KEYCLOAK_CLIENT}`,
+  'public-client': true,
+  'verify-token-audience': true,
+  'use-resource-role-mappings': true,
+  'confidential-port': 0,
   onLoad: 'login-required',
   // onLoad: 'check-sso',
 };
