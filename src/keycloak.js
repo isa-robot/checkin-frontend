96a75afe039074f3004953ea6dceae5f43420cd4
Matheus Frigo <matheus.tfrigo@gmail.com>
2020-07-26 21:37:16 -0300

    Variaveis de ambiente para inicializacao do keycloak

--- src/keycloak.js
+++ src/keycloak.js
@@ -3,25 +3,21 @@ import Keycloak from 'keycloak-js';
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 
-export const keycloak = new Keycloak();
+export const keycloak = Keycloak({
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT,
+  realm: process.env.REACT_APP_KEYCLOAK_REALM,
+  url: process.env.REACT_APP_KEYCLOAK_SERVER_URL,
+});
 
 export const keycloakProviderInitConfig = {
-  realm: 'isa-qualis',
-  'auth-server-url': `${process.env.KEYCLOAK_SERVER_URL}/`,
+  realm: process.env.REACT_APP_KEYCLOAK_REALM,
+  'auth-server-url': process.env.REACT_APP_KEYCLOAK_SERVER_URL,
   'ssl-required': 'external',
-  resource: `${process.env.KEYCLOAK_CLIENT}`,
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT,
+  resource: process.env.REACT_APP_KEYCLOAK_CLIENT,
   'public-client': true,
   'verify-token-audience': true,
   'use-resource-role-mappings': true,
   'confidential-port': 0,
   onLoad: 'login-required',
-  // onLoad: 'check-sso',
 };
-
-// export const onKeycloakEvent = (event, error) => {
-// console.log('onKeycloakEvent', event, error);
-// };
-
-// export const onKeycloakTokens = tokens => {
-// console.log('onKeycloakTokens', tokens);
-// };
