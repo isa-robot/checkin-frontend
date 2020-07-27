c2f22d78cdb3c834c3e73bff6cb74109e0f4dfd7
Adriano Boese <adriano@otus-solutions.com.br>
2020-07-27 18:30:42 -0300

    variaveis  de ambiente funcionando no container

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-envsubst '${REACT_APP_API_URL} ${KEYCLOAK_SERVER_URL} ${KEYCLOAK_CLIENT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf-updated
+envsubst '${REACT_APP_API_URL} ${REACT_APP_KEYCLOAK_SERVER_URL} ${KEYCLOAK_CLIENT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf-updated
+find /usr/share/nginx/html/static/js -type f -print0 | xargs -0 sed -i s,VAR_APP_KEYCLOAK_SERVER_URL,$REACT_APP_KEYCLOAK_SERVER_URL,g
 rm -rf /etc/nginx/conf.d/default.conf
 mv /etc/nginx/conf.d/default.conf-updated /etc/nginx/conf.d/default.conf
 exit 0;
